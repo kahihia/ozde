53b9095d1b6b25465ada9fd69a0ba20343ba3870
kalaiR <kalaimca.gs@gmail.com>
2015-08-13 17:18:36 +0530

    Revert "update settings paypal success url"
    This reverts commit a8a75aec02a47870050affc22bf55ad0ab0c044a.

--- oozaaoo_portals/oozaaoo_portals/settings.py
+++ oozaaoo_portals/oozaaoo_portals/settings.py
@@ -166,7 +166,7 @@ PAYU_INFO = {'merchant_key': "gtKFFx",
              'merchant_salt': "eCwWELxi",
              # for production environment use 'https://secure.payu.in/_payment'
              'payment_url': 'https://test.payu.in/_payment',
-             'surl':'http://localhost:8000/setprovisionalbooking/',
+             'surl':'http://localhost:8000/pay-success/',
              'furl':'http://localhost:8000/failure/',
              'curl':'http://localhost:8000/cancel/',
             }
