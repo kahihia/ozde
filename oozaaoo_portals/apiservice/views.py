d8ad020d845d5a232880c8e6e3950cb3e5066b54
spmuthu21 <muthu@globalensolutions.com>
2015-08-17 09:56:58 +0530

    multiple select seat map updated

--- oozaaoo_portals/apiservice/views.py
+++ oozaaoo_portals/apiservice/views.py
@@ -141,3 +141,31 @@ class goibiboAPI(object):
 
 	# def GetHotelPriceByCity(self, city_id, check_in, check_out):
 	# 	return (requests.get(self.BASE + "cyclone/" + "?city_id=%d" % city_id + "&check_in=%d" % check_in + "&check_out=%d" % check_out, params=self.auth).json())
+
+def format_redirect_url(redirect_path, query_string):
+    ''' utility to format redirect url with fixido query string
+    '''
+    stop_popup = True if 'st=' in query_string else False
+    
+    url_join_str = '?'
+    if url_join_str in redirect_path:
+        redirect_path, qs = redirect_path.split(url_join_str, 1)
+        query_string = qs + '&' + query_string
+    
+    qs = {}
+    for q in query_string.split('&'):
+        if '=' in q:
+            k, v = q.split('=', 1)
+            qs[k] = v
+    
+    if stop_popup:
+        if qs.has_key('zr'): del qs['zr']
+        if qs.has_key('lr'): del qs['lr']
+        if qs.has_key('ler'): del qs['ler']
+        if qs.has_key('thanks'): del qs['thanks']
+    
+    query_string = ''
+    for k in qs:
+        query_string += k + '=' + qs[k] + '&'
+        
+    return redirect_path + url_join_str + query_string[:-1]
