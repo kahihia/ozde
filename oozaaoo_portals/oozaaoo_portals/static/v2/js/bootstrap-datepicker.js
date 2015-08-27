3a235002c17752626ffd73fb0e0a4ece1cadf759
Spbala <priya@globalensolutions.com>
2015-08-27 19:26:12 +0530

    changes for adding new files in home page v2 integration

--- oozaaoo_portals/oozaaoo_portals/static/v2/js/bootstrap-datepicker.js
+++ oozaaoo_portals/oozaaoo_portals/static/v2/js/bootstrap-datepicker.js
@@ -1612,7 +1612,7 @@
     };
     DPGlobal.template = '<div class="datepicker">' +
         '<div class="datepicker-days">' +
-        '<table class=" table-condensed">' +
+        '<table class="table-condensed">' +
         DPGlobal.headTemplate +
         '<tbody></tbody>' +
         DPGlobal.footTemplate +
