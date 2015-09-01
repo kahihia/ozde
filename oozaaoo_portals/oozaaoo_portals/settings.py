ba129f7e0346d139e47e674d2299168dee09c490
saisasthaa <saisasthaa@gmail.com>
2015-09-01 10:33:05 +0530

    Search and Filter add for Hotel list

--- oozaaoo_portals/oozaaoo_portals/settings.py
+++ oozaaoo_portals/oozaaoo_portals/settings.py
@@ -51,8 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',    
 )
 
 ROOT_URLCONF = 'oozaaoo_portals.urls'
